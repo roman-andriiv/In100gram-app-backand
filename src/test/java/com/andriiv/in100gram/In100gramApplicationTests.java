08beda32a9c75a74cd4ab027332564c3a3d462ae
Roman Andriiv <roman.andriyiv@gmail.com>
2023-04-15 19:46:30 +0200

    Initialization commit

---
+++ src/test/java/com/andriiv/in100gram/In100gramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andriiv.in100gram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In100gramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
