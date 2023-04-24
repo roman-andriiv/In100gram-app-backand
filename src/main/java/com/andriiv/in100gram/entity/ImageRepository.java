afa2941896f538ebc117d0a21613474a191108c0
Roman Andriiv <roman.andriyiv@gmail.com>
2023-04-24 12:41:31 +0200

    [andriiv][server_dev_branch] add ImageRepository

---
+++ src/main/java/com/andriiv/in100gram/entity/ImageRepository.java
@@ -0,0 +1,16 @@
+package com.andriiv.in100gram.entity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/*
+Created by Roman Andriiv (24.04.2023 - 12:35)
+*/
+@Repository
+public interface ImageRepository extends JpaRepository<ImageModel, Long> {
+    Optional<ImageModel> findByUserId(Long userId);
+
+    Optional<ImageModel> findByPostId(Long postId);
+}
