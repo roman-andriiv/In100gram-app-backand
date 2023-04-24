dfad16f9ef3021d69aca68e0a154e109c737877e
Roman Andriiv <roman.andriyiv@gmail.com>
2023-04-24 12:20:00 +0200

    [andriiv][server_dev_branch] add UserRepository

---
+++ src/main/java/com/andriiv/in100gram/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.andriiv.in100gram.repository;
+
+import com.andriiv.in100gram.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+/*
+Created by Roman Andriiv (24.04.2023 - 12:11)
+*/
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findUserById(Long id);
+
+    Optional<User> findUserByUsername(String username);
+
+    Optional<User> findUserByEmail(String email);
+}
