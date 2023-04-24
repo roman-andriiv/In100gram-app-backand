8afaa713c3c81d5ae6bf0d4177b5a25bfa740f01
Roman Andriiv <roman.andriyiv@gmail.com>
2023-04-24 12:33:01 +0200

    [andriiv][server_dev_branch] add PostRepository

---
+++ src/main/java/com/andriiv/in100gram/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package com.andriiv.in100gram.repository;
+
+import com.andriiv.in100gram.entity.Post;
+import com.andriiv.in100gram.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+/*
+Created by Roman Andriiv (24.04.2023 - 12:23)
+*/
+@Repository
+public interface PostRepository  extends JpaRepository<Post, Long> {
+ Optional<Post> findAllByIdAndUser(Long id, User user);
+
+ List<Post> findAllByUserOrderByCreatedDateDesc(User user);
+ List<Post> findAllByOrderByCreatedDateDesc();
+}
