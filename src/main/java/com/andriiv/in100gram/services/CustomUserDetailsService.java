faf6f6dd899014d4f34733af42f34c6be5ad727e
Roman Andriiv <roman.andriyiv@gmail.com>
2023-04-26 10:30:06 +0200

    [andriiv][server_dev_branch][service] add CustomUserDetailsService

---
+++ src/main/java/com/andriiv/in100gram/services/CustomUserDetailsService.java
@@ -0,0 +1,50 @@
+package com.andriiv.in100gram.services;
+
+import com.andriiv.in100gram.entities.User;
+import com.andriiv.in100gram.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+*Created by Roman Andriiv (26.04.2023 - 09:56)
+*/
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+        User user = userRepository.findUserByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User are not found with such username: " + username));
+
+        return build(user);
+    }
+
+    public User loadUserById(Long id){
+        return userRepository.findUserById(id).orElse(null);
+    }
+
+    public static User build(User user) {
+        List<GrantedAuthority> authorities = user.getRole().stream()
+                .map(role -> new SimpleGrantedAuthority(role.name()))
+                .collect(Collectors.toList());
+
+        return new User(
+                user.getId(), user.getUsername(), user.getEmail(), user.getPassword(), authorities);
+    }
+}
