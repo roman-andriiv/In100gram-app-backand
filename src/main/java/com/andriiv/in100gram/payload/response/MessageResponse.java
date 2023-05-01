6af534156157f77d0b4b72ea2fac1492ca06bc65
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-01 13:27:35 +0200

    [andriiv][server_dev_branch][security] add AuthController and custom payload responses

---
+++ src/main/java/com/andriiv/in100gram/payload/response/MessageResponse.java
@@ -0,0 +1,13 @@
+package com.andriiv.in100gram.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * Created by Roman Andriiv (01.05.2023 - 12:12)
+ */
+@Data
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
