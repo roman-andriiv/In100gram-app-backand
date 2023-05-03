886185cbb7645d7a4918f34474a789cbef8eb103
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-03 10:17:36 +0200

    [andriiv][server_dev_branch][controllers] create and implement project Facades for mapping objects to client

---
+++ src/main/java/com/andriiv/in100gram/facade/CommentFacade.java
@@ -0,0 +1,22 @@
+package com.andriiv.in100gram.facade;
+
+import com.andriiv.in100gram.dto.CommentDTO;
+import com.andriiv.in100gram.entities.Comment;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by Roman Andriiv (03.05.2023 - 10:13)
+ */
+@Component
+public class CommentFacade {
+
+    public CommentDTO commentToCommentDTO(Comment comment){
+
+        CommentDTO commentDTO = new CommentDTO();
+        commentDTO.setId(comment.getId());
+        commentDTO.setUsername(comment.getUserName());
+        commentDTO.setMessage(comment.getMessage());
+
+        return commentDTO;
+    }
+}
